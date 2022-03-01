cad66664625b35b925a50a382349bb273deaebf3
SantosPereira <pedrohenriquelemam@gmail.com>
2022-03-01 19:39:25 -0300

    Preenchimento do controller com código boilerplate

---
+++ src/controller/FinanceiroController.py
@@ -0,0 +1,21 @@
+import json
+from flask import request
+from flask_restful import Resource
+
+
+class Financeiro(Resource):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def get(self):
+        return ''
+
+    def post(self):
+        dados = json.loads(request.data)
+        return ''
+
+    def put(self):
+        return ''
+
+    def delete(self):
+        return ''
