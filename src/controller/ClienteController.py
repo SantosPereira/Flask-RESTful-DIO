0c89db490c272c560156cfba0fd73c4cef41a08b
SantosPereira <pedrohenriquelemam@gmail.com>
2022-12-30 12:33:01 -0300

    fix: Alteração na formatação do arquivo

--- src/controller/ClienteController.py
+++ src/controller/ClienteController.py
@@ -8,21 +8,17 @@ class ClienteController(Resource):
     def __init__(self) -> None:
         super().__init__()
 
-
     @auth.login_required
     def get(self):
         return make_response(ClienteService.listar_clientes(self))
-
 
     @auth.login_required
     def post(self):
         return make_response(ClienteService.adicionar_cliente(self, request.data))
 
-
     @auth.login_required
     def put(self):
         return make_response(ClienteService.modificar_cliente(self, request.data))
-
 
     @auth.login_required
     def delete(self):
--- src/controller/VendaController.py
+++ src/controller/VendaController.py
@@ -8,20 +8,16 @@ class VendaController(Resource):
     def __init__(self) -> None:
         super().__init__()
 
-
     def get(self):
         return make_response(VendaService.listar_vendas(self))
-
 
     @auth.login_required
     def post(self):
         return make_response(VendaService.adicionar_venda(self, request.data))
 
-
     @auth.login_required
     def put(self):
         return make_response(VendaService.modificar_venda(self, request.data))
-
 
     @auth.login_required
     def delete(self):
